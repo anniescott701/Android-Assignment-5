cee95d73640ed602ec2094bb1c887714c60ca4e5
anniescott701 <anniescott701@gmail.com>
2018-04-10 20:43:22 +0530

    intents and its working

---
+++ app/src/main/java/com/scott/annie/intents/googlepage.java
@@ -0,0 +1,13 @@
+package com.scott.annie.intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class googlepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlepage);
+    }
+}
